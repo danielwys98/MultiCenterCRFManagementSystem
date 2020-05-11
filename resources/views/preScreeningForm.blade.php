19d8f319d8c9061ed192cb0de81f5600c1f3f123
MarcusLai07 <37129492+MarcusLai07@users.noreply.github.com>
2020-05-11 18:57:04 +0800

    Update preScreeningForm.blade.php
    add a btn

--- resources/views/preScreeningForm.blade.php
+++ resources/views/preScreeningForm.blade.php
@@ -5,7 +5,6 @@
    <hr>
 
     {!! Form::open(['url' => 'foo/bar']) !!}
-
     {{-- general consent --}}
     <div class="form-group">
         <h3>General Consent</h3>
@@ -1262,7 +1261,7 @@
             </div>
         </div>
     </div>
-
+    {!! Form::submit('Create',['class'=>'btn btn-primary'])!!}
     {!! Form::close() !!}
     
     
