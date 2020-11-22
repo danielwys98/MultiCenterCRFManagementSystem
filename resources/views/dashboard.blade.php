d266e2aca08d9a55f1b8818cd24d3e0457a44c4c
Daniel Wong <danielwong9898@gmail.com>
2020-11-23 01:33:05 +0800

    fix some views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
     <h1>Currently undergoing studies</h1>
-    <div class="card-deck">
     @foreach($studies as $s)
-        <div class="card">
-        <div class="card-header">
-            <p class="card-title"><a href={{ url('studySpecific/input/'.$s->study_id) }}>{{$s->study_name}}</a></p>
-        </div>
-        <div class="card-body">
-                    <p class="card-text">Has {{$s->studyPeriod_Count}} Study Periods</p>
-                    <p class="card-text">Start Date: {{$s->startDate}}</p>
-                    <p class="card-text">End Date: {{$s->endDate}}</p>
-        </div>
-        </div>
+         <div class="card" id="dashboard">
+            <div class="card-header">
+                <p class="card-title"><a href={{ url('studySpecific/input/'.$s->study_id) }}>{{$s->study_name}}</a></p>
+            </div>
+            <div class="card-body">
+                        <p class="card-text">Has {{$s->studyPeriod_Count}} Study Periods</p>
+                        <p class="card-text">Start Date: {{$s->startDate}}</p>
+                        <p class="card-text">End Date: {{$s->endDate}}</p>
+            </div>
+         </div>
         <br/>
     @endforeach
-    </div>
 
 @endsection
