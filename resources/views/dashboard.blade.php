af1427afe8756f2347f75ea666e92daf7d930ea1
Daniel Wong <danielwong9898@gmail.com>
2020-11-27 13:01:56 +0800

    Fix some views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,16 @@
 
 @section('content')
     <h1>Currently undergoing studies</h1>
-    @foreach($studies as $s)
-         <div class="card" id="dashboard">
-            <div class="card-header">
-                <p class="card-title"><a href={{ url('studySpecific/input/'.$s->study_id) }}>{{$s->study_name}}</a></p>
-            </div>
-            <div class="card-body">
-                        <p class="card-text">Has {{$s->studyPeriod_Count}} Study Periods</p>
-                        <p class="card-text">Start Date: {{$s->startDate}}</p>
-                        <p class="card-text">End Date: {{$s->endDate}}</p>
-            </div>
-         </div>
-        <br/>
-    @endforeach
-
+        @foreach($studies as $s)
+           <div class="card-columns-fluid" id="dashboard">
+                <div class="card-header">
+                    <p class="card-title"><a href={{ url('studySpecific/input/'.$s->study_id) }}>{{$s->study_name}}</a></p>
+                </div>
+                <div class="card-body">
+                            <p class="card-text">Has {{$s->studyPeriod_Count}} Study Periods</p>
+                            <p class="card-text">Start Date: {{$s->startDate}}</p>
+                            <p class="card-text">End Date: {{$s->endDate}}</p>
+                </div>
+             </div>
+        @endforeach
 @endsection
